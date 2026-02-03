0afbe80acd376e6806be3ae3a74031b3e736fbae
JessePW2025 <jessevieira@pi.senac.br>
2026-02-03 18:48:09 -0300

    test

--- conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "imobiliaria");
-
-if (!$conn) {
-    die("Erro de conexão");
-}
-?>
--- excluir.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conexao.php";
-
-$id = $_GET['id'];
-
-mysqli_query($conn, "DELETE FROM clientes WHERE id=$id");
-
-echo "<script> alert('Usuário excluído com sucesso!'); window.location='index.html'; </script>";
-?>
